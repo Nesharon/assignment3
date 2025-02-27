13be161235314013d84034d1970cd3a87c315694
Nesharon <71140265+Nesharon@users.noreply.github.com>
2025-02-27 10:49:07 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-import * as azure from "@pulumi/azure-native";
+import * as azureNative from "@pulumi/azure-native";
 
 
 // Configuration
