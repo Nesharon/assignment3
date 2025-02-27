a5a3d369d0dbf54cf3bc83d71e93757799c5df1c
Nesharon <71140265+Nesharon@users.noreply.github.com>
2025-02-27 15:39:48 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,148 @@ import * as azureNative from "@pulumi/azure-native";
 const config = new pulumi.Config();
 const location = config.get("location") || "uaenorth";
 
+// Create Resource Groupimport * as pulumi from "@pulumi/pulumi";
+import * as azure from "@pulumi/azure";
+import * as azureNative from "@pulumi/azure-native";
+
+// Configuration
+const config = new pulumi.Config();
+const location = config.get("location") || "uaenorth";
+
 // Create Resource Group
+const resourceGroup = new azure.core.ResourceGroup("aks-rg-s5", {
+    location: location,
+});
+
+// Virtual Network
+const vnet = new azure.network.VirtualNetwork("aks-vnet", {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    addressSpaces: ["10.1.0.0/16"],
+});
+
+// Subnet
+const subnet = new azure.network.Subnet("aks-subnet", {
+    resourceGroupName: resourceGroup.name,
+    virtualNetworkName: vnet.name,
+    addressPrefixes: ["10.1.1.0/24"],
+});
+
+// Public IP
+const publicIp = new azure.network.PublicIp("appgw-public-ip", {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    allocationMethod: "Static",
+    sku: "Standard",
+});
+
+// WAF Policy
+const wafPolicy = new azureNative.network.WebApplicationFirewallPolicy("wafPolicy", {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    policySettings: {
+        mode: "Prevention",
+        requestBodyCheck: true,
+    },
+    managedRules: {
+        managedRuleSets: [{
+            ruleSetType: "OWASP",
+            ruleSetVersion: "3.2",
+        }],
+    },
+});
+
+// Application Gateway
+const appGateway = new azure.network.ApplicationGateway("appGateway", {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    sku: {
+        name: "WAF_v2",
+        tier: "WAF_v2",
+        capacity: 2,
+    },
+    firewallPolicyId: wafPolicy.id.apply(id =>
+        id.replace("/ApplicationGatewayWebApplicationFirewallPolicies/", "/applicationGatewayWebApplicationFirewallPolicies/")
+    ),
+    gatewayIpConfigurations: [{
+        name: "appGwIPConfig",
+        subnetId: subnet.id,
+    }],
+    frontendIpConfigurations: [{
+        name: "appGwFrontendIPConfig",
+        publicIpAddressId: publicIp.id,
+    }],
+    frontendPorts: [{
+        name: "httpPort",
+        port: 80,
+    }],
+    backendAddressPools: [{
+        name: "appGwBackendPool",
+    }],
+    backendHttpSettings: [{
+        name: "httpSettings",
+        port: 80,
+        protocol: "Http",
+        requestTimeout: 20,
+        cookieBasedAffinity: "Disabled",
+    }],
+    httpListeners: [{
+        name: "httpListener",
+        frontendIpConfigurationName: "appGwFrontendIPConfig",
+        frontendPortName: "httpPort",
+        protocol: "Http",
+    }],
+    requestRoutingRules: [{
+        name: "rule1",
+        ruleType: "Basic",
+        httpListenerName: "httpListener",
+        backendAddressPoolName: "appGwBackendPool",
+        backendHttpSettingsName: "httpSettings",
+        priority: 100,
+    }],
+});
+
+// AKS Cluster
+const aksCluster = new azure.containerservice.KubernetesCluster("aks-cluster", {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    dnsPrefix: "myaks",
+    defaultNodePool: {
+        name: "agentpool",
+        nodeCount: 2,
+        vmSize: "Standard_D2_v2",
+        vnetSubnetId: subnet.id,
+    },
+    identity: { type: "SystemAssigned" },
+    networkProfile: {
+        networkPlugin: "azure",
+        serviceCidr: "10.0.0.0/16",
+    },
+    ingressApplicationGateway: {
+        gatewayId: appGateway.id,
+    },
+}, { dependsOn: [appGateway] });
+
+// AKS Credentials
+const creds = pulumi
+    .all([resourceGroup.name, aksCluster.name])
+    .apply(([rgName, aksName]) =>
+        azureNative.containerservice.listManagedClusterUserCredentials({
+            resourceGroupName: rgName,
+            resourceName: aksName,
+        })
+    );
+
+const kubeconfig = creds.apply(c => {
+    const encoded = c.kubeconfigs?.[0]?.value || "";
+    return Buffer.from(encoded, "base64").toString();
+});
+
+// Exports
+export const kubeconfigSecret = pulumi.secret(kubeconfig);
+export const appGatewayIp = publicIp.ipAddress;
+export const aksClusterName = aksCluster.name;
+
 const resourceGroup = new azure.core.ResourceGroup("aks-rg-s5", {
     location: location,
 });
